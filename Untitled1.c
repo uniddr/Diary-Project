6c6a934c9893370cfc989cc8ade807db813f386d
sh-26 <55090531+sh-26@users.noreply.github.com>
2019-09-12 10:36:47 -0700

    Update Untitled1.c

--- Untitled1.c
+++ Untitled1.c
@@ -1,114 +1,124 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
-#include<conio.h>
 #include<string.h>
+#include<dir.h>
 #include<dirent.h>
-#include<sys/stat.h>
-
+#include<process.h>
+#include<time.h>
 void addnote();
-void editnote();
 typedef struct entry
 {
     char date[12];
     char time[12];
-    char title[200];
+    char title[100];
     char body[1000];
-}entry;
-
+}
+entry;
 void addnote()
 {
-    system("cls");
-    int ch,i;
-    entry e;
-    FILE *fp;
-    DIR *entdir;
-    struct dirent *subdirread;
-    char buffer1[25]={"./"},buffer2[25]={"./"};
+      system("cls");
+      int ch1,i=92,value;
+      entry e;
+      FILE *fp;
+      char pathname[50],*entire_path,*entire_dir;
+
     printf("\n\t\t\t\t\t\t     ADD MENU\t\t\t\t\t  \n");
     printf("\t\t\t\t    --------------------------------------------\t\t\t\t  ");
     printf("\n\t\t\t\t     0   >>   To use system date and time\t\t\t\t\t\t  ");
     printf("\n\t\t\t\t     1   >>   To use custom date and time\t\t\t\t\t\t  \n");
     printf("\n\t\t\t\t     Enter your choice:  ");
-    scanf("%d",&ch);
-    if(ch==0)
+    scanf("%d",&ch1);
+    if(ch1==0)
     {
         time_t timer;
         char rtime[12]="";
         struct tm* tm_info;
 
-        time(&timer);
-        tm_info = localtime(&timer);
+        time(&timer);//retrieves the system date+time
+        tm_info = localtime(&timer);//returns a convertible system date+time
         strftime(rtime,sizeof(rtime), "%d-%m-%Y", tm_info);
         strcpy(e.date, rtime);
-        strftime(rtime,sizeof(rtime), "%H-%M", tm_info);
+        strftime(rtime,sizeof(rtime), "%H:%M", tm_info);
         strcpy(e.time, rtime);
     }
-    else if(ch==1)
+    else if(ch1==1)
     {
         fflush(stdin);
-        printf("Enter date(dd-mm-yyyy): ");
+        printf("Enter date(dd-mm-yyyy(Finish with %c)): ",i);
         gets(e.date);
         fflush(stdin);
     }
-    strcat(buffer1,e.date);
-    strcat(buffer1,"/");
-    if((entdir=opendir(buffer1))==NULL)
-    {
-        if(!(mkdir(buffer1)))
+      printf("Please enter path name (Finish with %c): ",i);
+      gets(pathname);
+      entire_dir=strcat(pathname,e.date);
+      value=mkdir(entire_dir);
+      if(!value)
+      {
+          printf("This directory has been created.\n");
+      }
+      else
         {
-            printf("Directory successfully created!");
-            entdir=opendir(buffer1);
+            printf("This entry already exists.Use Edit Entry from menu to edit the existing entry or delete the entry first!");
         }
-        else
+                if(ch1==1)
         {
-            printf("Failed to create directory!");
+            printf("Enter time(hh-mm): ");
+            fflush(stdin);
+            gets(e.time);
         }
-    }
-    printf("\nSuccessfully opened entry directory!\n");
-    if(ch==1)
-    {
-        printf("Enter time(hh-mm): ");
-        gets(e.time);
-    }
-    strcat(buffer2,e.date);
-    strcat(buffer2,"/");
-    strcat(buffer2,e.time);
-    if((fp=fopen(buffer2,"rb+"))!=NULL)
-    {
-        printf("This entry already exists.Use Edit Option from menu to edit the existing entry or delete the entry first!\n\nPress any key to go back to main menu");
-        _getch();
-    }
-    else
-    {
-        if((fp=fopen(buffer2,"wb+"))==NULL)
+        entire_path=strcat(entire_dir,e.time);
+        fp=fopen(entire_path,"w");
+        if(fp==NULL)
         {
-            printf("Unable to create entry!");
-            _getch();
+            printf("Cannot open file.\n");
+        }
+            printf("Enter title: ");
+            fflush(stdin);
+            gets(e.title);
+            fputs(e.title,fp);
+            fputs("\n",fp);
+            printf("Enter the note: ");
+            fflush(stdin);
+            gets(e.body);
+            fputs(e.body,fp);
             return;
-        }
-        printf("Enter title: ");
-        fflush(stdin);
-        gets(e.title);
-        fputs(e.title,fp);
-        fputs("\n",fp);
-        for(i=0;i<strlen(e.title);i++)
-        {
-            fputs("-",fp);
-        }
-        fputs("---\n",fp);
-        printf("\nEnter the note: ");
-        fflush(stdin);
-        gets(e.body);
-        fputs(e.body,fp);
-        fclose(fp);
-        closedir(entdir);
-        printf("\n\nEntry created successfully! Press any key to go back to main menu ");
-        _getch();
-    }
+
+
 }
+void viewnote()
+{
+     int i=92;
+     struct dirent *dir_name;
+     FILE *fp;
+     DIR *dp;
+     char pathname[50],filename[50];
+     printf("Enter path name (Finish with %c): ",i);
+     fflush(stdin);
+     gets(pathname);
+     dp=opendir(pathname);
+     if(dp==NULL)
+     {
+         printf("This directory does not exist.\n");
+     }
+     else
+     {
+         printf("Files exist in %s : \n",pathname);
+         while((dir_name=readdir(dp))!=NULL)
+         {
+             printf("%s\n",dir_name->d_name);
+         }
+     }
+     printf("Enter file name : ");
+     gets(filename);
+     fp=fopen(strcat(pathname,filename),"r");
+     if(fp==NULL)
+     {
+         printf("File does not exist.\n");
+     }
+     printf("Success!!!File has been found.\n");
 
 
+}
 int main(int argc,char *argv[])
 {
     int t,j,c;
@@ -119,7 +129,8 @@ int main(int argc,char *argv[])
         printf("\n\t\t\t\t    --------------------------------------------\t\t\t\t  ");
         printf("\n\t\t\t\t\t\t     Main Menu\t\t\t\t\t  \n");
         printf("\n\t\t\t\t     0   >>   Exit Program\t\t\t\t\t\t  ");
-        printf("\n\t\t\t\t     1   >>   Add Record\t\t\t\t\t\t  \n");
+        printf("\n\t\t\t\t     1   >>   Add Record\t\t\t\t\t\t\t  ");
+        printf("\n\t\t\t\t     2   >>   View Record\t\t\t\t\t\t  \n");
         printf("\n\t\t\t\t     Enter your choice:  ");
         scanf("%d",&c);
 
@@ -131,14 +142,16 @@ int main(int argc,char *argv[])
         case 1:
             addnote();
             break;
-        /*case 2:
-            editnote();
+        case 2:
+            viewnote();
             break;
-        */
+
         default:
             printf("\nWrong choice!");
             break;
         }
         system("cls");
     }
+    return 0;
 }
+

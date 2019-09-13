aefb0043848f03931f1ac9404b9e57575087b137
sh-26 <55090531+sh-26@users.noreply.github.com>
2019-09-13 12:12:10 -0700

    Update Project.c

--- Project.c
+++ Project.c
@@ -7,6 +7,26 @@
 #include<time.h>
 void addnote();
 void viewnote();
+int fcount(char *dir_name)
+{
+     int filecount=-2;
+     struct dirent *dir;
+     DIR *dp;
+     dp=opendir(dir_name);
+     if(dp==NULL)
+     {
+         printf("Cannot open directory.\n");
+     }
+     while((dir=readdir(dp))!=NULL)
+     {
+      if((dir->d_name)!=NULL)
+          {
+              filecount++;
+          }
+     }
+      return filecount;
+
+}
 typedef struct entry
 {
     char date[12];
@@ -17,12 +37,14 @@ typedef struct entry
 entry;
 void addnote()
 {
-      system("cls");
-      int ch1,i=92,value;
-      entry e;
-      FILE *fp;
-      char pathname[50],*entire_path,*entire_dir;
-
+    system("cls");
+    struct dirent *dir;
+    entry e;
+    DIR *dp;
+    FILE *fp;
+    int value,in,i=92,ch1,file_count;
+    char dirname[50],*entire_dir,*entire_path,*entire_file,s_num[50];
+    char path_name[50],filename[50],str[50];
     printf("\n\t\t\t\t\t\t     ADD MENU\t\t\t\t\t  \n");
     printf("\t\t\t\t    --------------------------------------------\t\t\t\t  ");
     printf("\n\t\t\t\t     0   >>   To use system date and time\t\t\t\t\t\t  ");
@@ -44,31 +66,34 @@ void addnote()
     }
     else if(ch1==1)
     {
+        printf("Enter a valid path name(Finish with %c) : ",i);
         fflush(stdin);
+        gets(path_name);
         printf("Enter date(dd-mm-yyyy(Finish with %c)): ",i);
+        fflush(stdin);
         gets(e.date);
         fflush(stdin);
+        entire_dir=strcat(path_name,e.date);
+        value=mkdir(entire_dir);
+    if(!value)
+    {
+        printf("Directory has been created.\n");
     }
-      printf("Please enter path name (Finish with %c): ",i);  //*Initial path is needed*//
-      gets(pathname);
-      entire_dir=strcat(pathname,e.date);
-      value=mkdir(entire_dir);
-      if(!value)
-      {
-          printf("This directory has been created.\n");
-      }
-      else
+    else
         {
-            printf("This entry already exists.Use Edit Entry from menu to edit the existing entry or delete the entry first!");
+            printf("This directory already exists.\n");
         }
-                if(ch1==1)
-        {
-            printf("Enter time(hh-mm): ");
-            fflush(stdin);
-            gets(e.time);
-        }
-        entire_path=strcat(entire_dir,e.time);
-        fp=fopen(entire_path,"wb+");
+    if(fcount(entire_dir)==0)
+    {
+        printf("This directory is empty.\n");
+        file_count=fcount(entire_dir);
+        printf("Enter a name to create %d-th file [hh-mm] : ",file_count+1);
+        sprintf(s_num,"%d",file_count+1);
+        fflush(stdin);
+        gets(e.time);
+        entire_file=strcat(s_num,e.time);
+        entire_path=strcat(entire_dir,entire_file);
+        fp=fopen(entire_path,"w+");
         if(fp==NULL)
         {
             printf("Cannot open file.\n");
@@ -84,47 +109,89 @@ void addnote()
             fputs(e.body,fp);
             rewind(fp);
 
-
+    }
+       else
+    {
+        dp=opendir(entire_dir);
+        if(dp==NULL)
+        {
+            printf("Cannot open directory.\n");
+        }
+        while((dir=readdir(dp))!=NULL)
+        {
+            printf("%s\n",dir->d_name);
+        }
+        file_count=fcount(entire_dir);
+        printf("Enter a name to create %d-th file [hh-mm]: ",file_count+1);
+        sprintf(s_num,"%d",file_count+1);
+        fflush(stdin);
+        gets(e.time);
+        entire_file=strcat(s_num,e.time);
+        entire_path=strcat(entire_dir,entire_file);
+        fp=fopen(entire_path,"w+");
+        if(fp==NULL)
+        {
+            printf("Cannot open file.\n");
+        }
+            printf("Enter title: ");
+            fflush(stdin);
+            gets(e.title);
+            fputs(e.title,fp);
+            fputs("\n",fp);
+            printf("Enter the note: ");
+            fflush(stdin);
+            gets(e.body);
+            fputs(e.body,fp);
+            rewind(fp);
+    }
+}
 }
 void viewnote()
 {
-     int i=92;
-     struct dirent *dir_name;
-     FILE *fp;
-     DIR *dp;
-     char pathname[50],filename[50];
-     printf("Enter path name (Finish with %c): ",i);
-     fflush(stdin);
-     gets(pathname);
-     dp=opendir(pathname);
-     if(dp==NULL)
-     {
-         printf("This directory does not exist.\n");
-     }
-     else
-     {
-         printf("Files exist in %s : \n",pathname);
-         while((dir_name=readdir(dp))!=NULL)
-         {
-             printf("%s\n",dir_name->d_name);
-         }
-     }
-     printf("Enter file name : ");
-     gets(filename);
-     fp=fopen(strcat(pathname,filename),"rb+");
-     if(fp==NULL)
-     {
-         printf("File does not exist.\n");
-     }
-     while(1)
-     {
-         char ch=fgetc(fp);
-         if(feof(fp))
-         {
-             break;
-         }
-         putchar(ch);
-     }
+    int i=92,in,file_num;
+    FILE *fp;
+    struct dirent *dir;
+    DIR *dp;
+    char pathname[50],dirname[50],*entire_path,*entire_dir,str[50],*filename;
+    printf("Enter a valid path name(Finish with %c) : ",i);
+    fflush(stdin);
+    gets(pathname);
+    printf("Enter date(dd-mm-yyyy(Finish with %c)): ",i);
+    fflush(stdin);
+    gets(dirname);
+    entire_dir=strcat(pathname,dirname);
+    dp=opendir(entire_dir);
+    if(dp==NULL)
+    {
+        printf("Cannot open directory.\n");
+    }
+    printf("Total files -> %d\n",fcount(entire_dir));
+    while((dir=readdir(dp))!=NULL)
+    {
+            printf("%s\n",dir->d_name);
+    }
+    printf("Enter file serial number [Please enter the first digit from left] : ");
+    scanf("%d",&file_num);
+    sprintf(str,"%d",file_num);
+    dp=opendir(entire_dir);
+    while((dir=readdir(dp))!=NULL)
+    {
+        if(strncmp(str,(dir->d_name),1)==0)
+        {
+            printf("%s\n",dir->d_name);
+            filename=dir->d_name;
+        }
+    }
+    entire_path=strcat(entire_dir,filename);
+    fp=fopen(entire_path,"r+");
+    if(fp==NULL)
+    {
+        printf("Cannot read file.\n");
+    }
+    else
+    {
+        printf("File has been found.\n");
+    }
 
 }
 int main(int argc,char *argv[])
@@ -158,8 +225,6 @@ int main(int argc,char *argv[])
             printf("\nWrong choice!");
             break;
         }
-        system("cls");
     }
     return 0;
 }
-
